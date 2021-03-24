a3867334b36d9140396452e6d165c8aca71c592c
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-24 13:24:27 -0300

    Improve grid

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,13 +3,9 @@ import { darken } from 'polished';
 
 export const ProductList = styled.ul`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   grid-gap: 20px;
   list-style: none;
-
-  @media(max-width: 768px) {
-    grid-template-columns: 1fr;
-  }
 
   li {
     display: flex;
